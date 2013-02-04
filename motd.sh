0702baa8460c1084d0aef339b2ca40398a256bd2
Kyle Hornberg <kyle.hornberg@gmail.com>
2013-02-03 20:03:53 -0600

    Updated directory so that it is not hard coded. Just run the script and it works.

--- motd.sh
+++ motd.sh
@@ -4,6 +4,8 @@ verse=""
 # varible for the message or verse content
 message=""
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 function getContentFromBiblegateway ()
 {
   # curl the verse of the day
@@ -11,21 +13,21 @@ function getContentFromBiblegateway ()
   # wait 10 seconds for timeout
   # output the rss file to the project directory 
   # wait for curl to complete before continuing in the script
-  curl -s --connect-timeout 10 -o ~/Projects/motd/votd http://www.biblegateway.com/votd/get/?format=atom&version=ESV
+  curl -s --connect-timeout 10 -o $votd_directory http://www.biblegateway.com/votd/get/?format=atom&version=ESV
   wait
 }
 
 # Pass rss file through sed to get the message and verse reference
 function sendToScript ()
 {
-  message=`sed -n '/&ldquo;.*&/ p' ~/Projects/motd/votd`
-  verse=`sed -n '14 p' ~/Projects/motd/votd`
-  ~/Projects/motd/votd.pl "$verse" "$message" 
+  message=`sed -n '/&ldquo;.*&/ p' $DIR/votd`
+  verse=`sed -n '14 p' $DIR/votd`
+  $DIR/votd.pl "$verse" "$message" 
 }
 
 if [ -f votd ]
   then
-    statDate=`stat -c %y ~/Projects/motd/votd | awk '{print $1}'`
+    statDate=`stat -c %y $DIR/votd | awk '{print $1}'`
     if [[ $statDate != `date +"%Y-%m-%d"` ]]
       then
         getContentFromBiblegateway
